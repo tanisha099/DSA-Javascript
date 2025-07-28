b68ef6319c15f86b8210c81ccbb3498ae8f99db7
Tanisha Muvel <tanimuvel1234@gmail.com>
2025-07-28 23:37:15 +0530

    Map-Question

---
+++ set-question/a2.js
@@ -0,0 +1,7 @@
+let n = [193];
+let str = n.toString();
+
+for(i=0; i<str.length; i++){
+    let square = str[i]**2;
+     console.log(square);
+}
--- set-question/q1.js
+++ set-question/q1.js
@@ -1,4 +1,5 @@
  
+ /*//check-if-the-sentence-is-pangram -> all alphabet ane chaiye dublicate chalega
 let sentence = "thequickbrownfoxjumpsoverthelazydog"
 var checkIfPangram = function(sentence) {
     let set =new Set ();
@@ -6,5 +7,22 @@ var checkIfPangram = function(sentence) {
         let ch = sentence.charAt(i);
         set.add(ch);
     }
-    return set.size === 26
+    return set.size === 26 //false
 };
+*/
+// 
+let jewels = "aA";
+let stones = "aAAbbbb";
+
+var numJewelsInStones = function(jewels, stones) {
+    let set = new Set();
+    for(let i=0; i<jewels.length; i++){
+        set.add(jewels.charAt(i));
+    }
+    count =0;
+    for(let i=0; i<stones.length; i++){
+        let ch = stones.charAt(i);
+        if(set.has(ch)) count++;
+    }
+    return count
+}
