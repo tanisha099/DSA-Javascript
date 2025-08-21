dc01567ec756cad620588acd2282ffea51e949d7
Tanisha Muvel <tanimuvel1234@gmail.com>
2025-08-21 21:24:48 +0530

    leetcode problem

--- Array-question/sorting/merge.js
+++ Array-question/sorting/merge.js
@@ -22,6 +22,7 @@ while(j<arr2.length){
 
 console.log(ans);
 */
+/*
 //leetcode problem 88 merge sorted array
 var merge = function(nums1, m, nums2, n) {
     let i = m-1 , j=n-1, k=nums1.length-1;
@@ -33,3 +34,44 @@ var merge = function(nums1, m, nums2, n) {
    nums1[k--] = nums2[j--] 
 }
 };
+*/
+/*
+// leetcode problem Maximum Subarray
+var maxSubArray = function(nums) {
+    let sum = 0, maxsum=nums[0];
+    for(i=0; i<nums.length; i++){
+       sum = Math.max(nums[i], sum + nums[i]);
+        maxsum = Math.max(maxsum, sum);
+    }
+    return maxsum;
+};
+*/
+/*
+// leetcode problem  Majority Element
+
+var majorityElement = function(nums) {
+   let count = 1, ans = nums[0];
+
+   for(i=1; i<nums.length; i++){
+    if(count == 0){
+        ans = nums[i]
+        count =1
+    }
+    else if (ans!= nums[i]) count -- ;
+    else count ++
+   }
+   return ans 
+};
+*/
+// leetcode problem 121
+/*
+var maxProfit = function(prices) {
+    let min=prices[0] , maxprofit=0 ;
+    for(i=1; i<prices.length; i++){
+     if(prices[i]<min) min = prices[i];
+    let profit = prices[i] -min;
+    maxprofit = Math.max(maxprofit , profit)
+    }
+    return maxprofit
+};
+*/
